1c2d6d9eab02800998edc0a6b5d3164ab7eec30c
Ashley <ashley@foxgenesis.net>
2023-08-25 21:29:39 -0500

    Update CatPlugin.java

--- src/net/foxgenesis/cats/CatPlugin.java
+++ src/net/foxgenesis/cats/CatPlugin.java
@@ -1,16 +1,17 @@
 package net.foxgenesis.cats;
 
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.Set;
 
 import net.foxgenesis.cats.listener.RandomCats;
 import net.foxgenesis.util.resource.ConfigType;
-import net.foxgenesis.watame.WatameBot;
-import net.foxgenesis.watame.plugin.CommandProvider;
 import net.foxgenesis.watame.plugin.IEventStore;
 import net.foxgenesis.watame.plugin.Plugin;
-import net.foxgenesis.watame.plugin.PluginConfiguration;
 import net.foxgenesis.watame.plugin.SeverePluginException;
+import net.foxgenesis.watame.plugin.require.CommandProvider;
+import net.foxgenesis.watame.plugin.require.PluginConfiguration;
+import net.foxgenesis.watame.plugin.require.RequiresIntents;
 
 import org.apache.commons.configuration2.Configuration;
 
@@ -19,13 +20,14 @@ import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 /**
  * @author Ashley
  *
  */
 @PluginConfiguration(defaultFile = "/META-INF/cats/settings.properties", identifier = "catSettings", outputFile = "cats/settings.properties", type = ConfigType.PROPERTIES)
-public class CatPlugin extends Plugin implements CommandProvider {
+public class CatPlugin extends Plugin implements CommandProvider, RequiresIntents {
 	private final String catAPIKey;
 
 	public CatPlugin() {
@@ -51,10 +53,10 @@ public class CatPlugin extends Plugin implements CommandProvider {
 	}
 
 	@Override
-	public void postInit(WatameBot bot) {}
+	public void postInit() {}
 
 	@Override
-	public void onReady(WatameBot bot) {}
+	public void onReady() {}
 
 	@Override
 	public void close() throws Exception {
@@ -74,5 +76,10 @@ public class CatPlugin extends Plugin implements CommandProvider {
 				Commands.slash("catupload", "Upload a picture of a cat").setGuildOnly(true)
 						.setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
 						.addOption(OptionType.ATTACHMENT, "file", "Cat picture to upload", true));
+	}
+
+	@Override
+	public EnumSet<GatewayIntent> getRequiredIntents() {
+		return EnumSet.of(GatewayIntent.GUILD_MESSAGES);
 	}
 }
